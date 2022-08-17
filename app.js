4a36d889c9af63d730c73692bdd64e89075258c6
Jordan <jkwan104@gmail.com>
2022-08-16 20:35:01 -0700

    Revised

--- app.js
+++ app.js
@@ -2,41 +2,103 @@
 
 console.log('app is running');
 
-let user = prompt('What is you name?');
+let user = prompt('What is your name?');
 console.log(user);
-if (user === null) {
+if (!user) {
   alert('You need to provide your name');
-  user = prompt('No really what is your name?');
+  user = prompt('What is your name?');
+}
+  else {
+  alert('Welcome ' + user + ' .' + ' Thank you for stopping by! ');
 }
 
-let color = prompt('What is your favorite color?');
 
-switch (color) {
-case 'red':
-  alert('I like red');
-  break;
-case 'blue':
-  alert('blue is great too');
-  break;
-case 'yellow':
-  alert('yellow is just okay');
-  break;
-default:
-  alert('Oh interesting, I don\'t know that color');
+// question 1
+let answer = prompt('Is the sky blue?'); // String data type
+if (typeof(answer) === 'string') {
+  answer = answer.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
 }
 
-// creates a variables and asks the user to give it a value.
-let movie = prompt('What is one of my favorite action movies?'); // String data type
-if (typeof(movie) === 'string') {
-  movie = movie.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+if (!answer) {
+  alert('Invalid answer, try again.');
+  answer = prompt('Yes or No.')
+}
+else if (answer == 'y' || 'yes') {
+  alert('Yes the sky is blue! ');
+}
+else {
+  alert('Please try again!');
+  answer = prompt('Is the sky blue?');
+}
+// question 2
+let answer1 = prompt('Is Earth in the Milkyway?'); // String data type
+if (typeof(answer1) === 'string') {
+  answer1 = answer1.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
 }
 
-if (movie === 'con air') {
-  alert('That is a great Movie!');
-} else if (movie === 'the rock') {
-  alert('Another great Movie');
-} else if (movie === 'face off') {
-  alert('One of my faves!');
-} else {
-  alert('I don\'t know that movie!');
+if (!answer1 ) {
+  alert('Invalid answer, try again.');
+  answer1 = prompt('Yes or No.')
 }
+else if (answer1 == 'y' || 'yes') {
+  alert('Yes Earth is in the MilkyWay! ');
+}
+else {
+  alert('Please try again!');
+  answer1 = prompt('Is Earth in the Milkyway?');
+}
+
+// question 3
+let answer2 = prompt('Is Godzilla large?'); // String data type
+if (typeof(answer2) === 'string') {
+  answer2 = answer2.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer2 ) {
+  alert('Invalid answer, try again.');
+  answer2 = prompt('Yes or No.')
+}
+else if (answer2 == 'y' || 'yes') {
+  alert('Yes Godzilla is large! ');
+}
+else {
+  alert('Please try again!');
+  answer2 = prompt('Is Godzilla large?');
+}
+
+// question 4
+let answer3 = prompt('Is the Sun hot?'); // String data type
+if (typeof(answer3) === 'string') {
+  answer3 = answer3.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer3 ) {
+  alert('Invalid answer, try again.');
+  answer3 = prompt('Yes or No.')
+}
+else if (answer3 == 'y' || 'yes') {
+  alert('Yes the Sun is hot! ');
+}
+else {
+  alert('Please try again!');
+  answer3 = prompt('Is the Sun hot?');
+}
+
+// question 5
+let answer4 = prompt('Is Antarctica cold?'); // String data type
+if (typeof(answer4) === 'string') {
+  answer4 = answer4.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer4 ) {
+  alert('Invalid answer, try again.');
+  answer4 = prompt('Yes or No.')
+}
+else if (answer4 == 'y' || 'yes') {
+  alert('Yes Antarctica is cold! ');
+}
+else {
+  alert('Please try again!');
+  answer4 = prompt('Is Antarctica cold?');
+}
+
