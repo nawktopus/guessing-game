08836974b9716e1e6a3a3b72549feb2e299d0664
Jordan <jkwan104@gmail.com>
2022-08-16 23:41:51 -0700

    Final Revision

--- app.js
+++ app.js
@@ -2,8 +2,125 @@
 
 console.log('app is running');
 
+// question 1
+let answer = prompt('Do I like cars?'); // String data type
+
+if (typeof(answer) === 'string') {
+  answer = answer.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer) {
+  alert('Invalid answer, try again.');
+  answer = prompt('Yes or No.')
+  alert('Yes, I do! ');
+}
+else if (answer === 'y') {
+  alert('Yes, I do! ');
+}
+else if (answer === 'yes') {
+  alert('Yes, I do! ');
+}
+else {
+  alert('Please try again!');
+  answer = prompt('Do I like cars?');
+  alert('Yes, I do! ');
+}
+//console.log(answer + ", I do like cars!");
+// question 2
+let answer1 = prompt('Am I a fan of boba?'); // String data type
+if (typeof(answer1) === 'string') {
+  answer1 = answer1.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer1 ) {
+  alert('Invalid answer, try again');
+  answer1 = prompt('Yes or No.')
+  alert('Yes, I am! ');
+}
+else if (answer1 === 'y') {
+  alert('Yes, I am! ');
+}
+else if (answer1 === 'yes') {
+  alert('Yes, I am! ');
+}
+else {
+  alert('Please try again!');
+  answer1 = prompt('Am I a fan of boba?');
+  alert('Yes, I am! ');
+}
+//console.log(answer1 + ", I do like boba!");
+// question 3
+let answer2 = prompt('Am I a fan of the Giants?'); // String data type
+if (typeof(answer2) === 'string') {
+  answer2 = answer2.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer2 ) {
+  alert('Invalid answer, try again.');
+  answer2 = prompt('Yes or No.')
+  alert('Yes, I am! ');
+}
+else if (answer2 === 'y') {
+  alert('Yes, I am! ');
+}
+else if (answer2 === 'yes') {
+  alert('Yes, I am! ');
+}
+else {
+  alert('Please try again!');
+  answer2 = prompt('Am I a fan of the Giants?');
+  alert('Yes, I am! ');
+}
+//console.log(answer2 + ", I am a Giants fan!");
+// question 4
+let answer3 = prompt('Do I like dogs?'); // String data type
+if (typeof(answer3) === 'string') {
+  answer3 = answer3.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer3 ) {
+  alert('Invalid answer, try again.');
+  answer3 = prompt('Yes or No.')
+  alert('Yes, I do! ');
+}
+else if (answer3 === 'y') {
+  alert('Yes, I do! ');
+}
+else if (answer3 === 'yes') {
+  alert('Yes, I do! ');
+}
+else {
+  alert('Please try again!');
+  answer3 = prompt('Do I like dogs?');
+  alert('Yes, I do! ');
+}
+//console.log(answer3 + ", I do like dogs!");
+// question 5
+let answer4 = prompt('Have I been to Japan?'); // String data type
+if (typeof(answer4) === 'string') {
+  answer4 = answer4.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
+}
+
+if (!answer4 ) {
+  alert('Invalid answer, try again.');
+  answer4 = prompt('Yes or No.')
+  alert('Yes, I have! ');
+}
+else if (answer4 === 'y') {
+  alert('Yes, I have! ');
+}
+else if (answer4 === 'yes') {
+  alert('Yes, I have! ');
+}
+else {
+  alert('Please try again!');
+  answer4 = prompt('Have I been to Japan?');
+  alert('Yes, I have! ');
+}
+//console.log(answer4 + ", I have gone to Japan!");
+
 let user = prompt('What is your name?');
-console.log(user);
+//console.log("Your name is " + user);
 if (!user) {
   alert('You need to provide your name');
   user = prompt('What is your name?');
@@ -12,114 +129,3 @@ if (!user) {
   else {
   alert('Welcome ' + user + ' .' + ' Thank you for stopping by! ');
 }
-
-
-// question 1
-let answer = prompt('Is the sky blue?'); // String data type
-if (typeof(answer) === 'string') {
-  answer = answer.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
-}
-
-if (!answer) {
-  alert('Invalid answer, try again.');
-  answer = prompt('Yes or No.')
-}
-else if (answer === 'y') {
-  alert('Yes, the sky is blue! ');
-}
-else if (answer === 'yes') {
-  alert('Yes, the sky is blue! ');
-}
-else {
-  alert('Please try again!');
-  answer = prompt('Is the sky blue?');
-  alert('Yes, the sky is blue! ');
-}
-// question 2
-let answer1 = prompt('Is Earth in the Milkyway?'); // String data type
-if (typeof(answer1) === 'string') {
-  answer1 = answer1.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
-}
-
-if (!answer1 ) {
-  alert('Invalid answer, try again.');
-  answer1 = prompt('Yes or No.')
-}
-else if (answer1 === 'y') {
-  alert('Yes, Earth is in the MilkyWay! ');
-}
-else if (answer1 === 'yes') {
-  alert('Yes, Earth is in the MilkyWay! ');
-}
-else {
-  alert('Please try again!');
-  answer1 = prompt('Is Earth in the Milkyway?');
-  alert('Yes, Earth is in the MilkyWay! ');
-}
-
-// question 3
-let answer2 = prompt('Is Godzilla large?'); // String data type
-if (typeof(answer2) === 'string') {
-  answer2 = answer2.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
-}
-
-if (!answer2 ) {
-  alert('Invalid answer, try again.');
-  answer2 = prompt('Yes or No.')
-}
-else if (answer2 === 'y') {
-  alert('Yes, Godzilla is large! ');
-}
-else if (answer2 === 'yes') {
-  alert('Yes, Godzilla is large! ');
-}
-else {
-  alert('Please try again!');
-  answer2 = prompt('Is Godzilla large?');
-  alert('Yes, Godzilla is large! ');
-}
-
-// question 4
-let answer3 = prompt('Is the Sun hot?'); // String data type
-if (typeof(answer3) === 'string') {
-  answer3 = answer3.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
-}
-
-if (!answer3 ) {
-  alert('Invalid answer, try again.');
-  answer3 = prompt('Yes or No.')
-}
-else if (answer3 === 'y') {
-  alert('Yes, the Sun is hot! ');
-}
-else if (answer3 === 'yes') {
-  alert('Yes, the Sun is hot! ');
-}
-else {
-  alert('Please try again!');
-  answer3 = prompt('Is the Sun hot?');
-  alert('Yes, the Sun is hot! ');
-}
-
-// question 5
-let answer4 = prompt('Is Antarctica cold?'); // String data type
-if (typeof(answer4) === 'string') {
-  answer4 = answer4.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
-}
-
-if (!answer4 ) {
-  alert('Invalid answer, try again.');
-  answer4 = prompt('Yes or No.')
-}
-else if (answer4 === 'y') {
-  alert('Yes, Antarctica is cold! ');
-}
-else if (answer4 === 'yes') {
-  alert('Yes, Antarctica is cold! ');
-}
-else {
-  alert('Please try again!');
-  answer4 = prompt('Is Antarctica cold?');
-  alert('Yes, Antarctica is cold! ');
-}
-
