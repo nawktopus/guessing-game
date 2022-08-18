737789fe4255d5d3f880966614b7ee319843b5ef
Jordan <jkwan104@gmail.com>
2022-08-18 02:03:36 -0700

    One more update til completeion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'; // strict syntax
 
 console.log('app is running');
+let count = 0;
 
 // question 1
 let answer = prompt('Do I like cars?'); // String data type
@@ -11,20 +12,20 @@ if (typeof(answer) === 'string') {
 
 if (!answer) {
   alert('Invalid answer, try again.');
-  answer = prompt('Yes or No.')
-  alert('Yes, I do! ');
+  answer = prompt('Yes or No.');
 }
 else if (answer === 'y') {
   alert('Yes, I do! ');
+  let count = count++;
 }
 else if (answer === 'yes') {
   alert('Yes, I do! ');
+  let count = count++;
 }
 else {
-  alert('Please try again!');
-  answer = prompt('Do I like cars?');
-  alert('Yes, I do! ');
+  alert('Incorrect');
 }
+
 //console.log(answer + ", I do like cars!");
 // question 2
 let answer1 = prompt('Am I a fan of boba?'); // String data type
@@ -34,19 +35,26 @@ if (typeof(answer1) === 'string') {
 
 if (!answer1 ) {
   alert('Invalid answer, try again');
-  answer1 = prompt('Yes or No.')
-  alert('Yes, I am! ');
+  answer1 = prompt('Yes or No.');
+  if (answer1 == 'no') {
+    alert('Incorrect');
+  }
+  else {
+    alert('Yes, I do! ');
+    let count = count++;
+  }
+    
 }
 else if (answer1 === 'y') {
   alert('Yes, I am! ');
+  let count = count++;
 }
 else if (answer1 === 'yes') {
   alert('Yes, I am! ');
+  let count = count++;
 }
 else {
-  alert('Please try again!');
-  answer1 = prompt('Am I a fan of boba?');
-  alert('Yes, I am! ');
+  alert('Incorrect.')
 }
 //console.log(answer1 + ", I do like boba!");
 // question 3
@@ -58,7 +66,13 @@ if (typeof(answer2) === 'string') {
 if (!answer2 ) {
   alert('Invalid answer, try again.');
   answer2 = prompt('Yes or No.')
-  alert('Yes, I am! ');
+  if (answer2 == 'no') {
+    alert('Incorrect');
+  }
+  else {
+    alert('Yes, I do! ');
+    let count = count++;
+  }
 }
 else if (answer2 === 'y') {
   alert('Yes, I am! ');
@@ -80,8 +94,14 @@ if (typeof(answer3) === 'string') {
 
 if (!answer3 ) {
   alert('Invalid answer, try again.');
-  answer3 = prompt('Yes or No.')
-  alert('Yes, I do! ');
+  answer3 = prompt('Yes or No.');
+  if (answer3 == 'no') {
+    alert('Incorrect');
+  }
+  else {
+    alert('Yes, I do! ');
+    let count = count++;
+  }
 }
 else if (answer3 === 'y') {
   alert('Yes, I do! ');
@@ -104,7 +124,13 @@ if (typeof(answer4) === 'string') {
 if (!answer4 ) {
   alert('Invalid answer, try again.');
   answer4 = prompt('Yes or No.')
-  alert('Yes, I have! ');
+  if (answer4 == 'no') {
+    alert('Incorrect');
+  }
+  else {
+    alert('Yes, I do! ');
+    let count = count++;
+  }
 }
 else if (answer4 === 'y') {
   alert('Yes, I have! ');
@@ -124,10 +150,10 @@ let user = prompt('What is your name?');
 if (!user) {
   alert('You need to provide your name');
   user = prompt('What is your name?');
-  alert('Welcome ' + user + ' .' + ' Thank you for stopping by! ');
+  alert('Welcome ' + user + '.' + ' Thank you for stopping by! ');
 }
   else {
-  alert('Welcome ' + user + ' .' + ' Thank you for stopping by! ');
+  alert('Welcome ' + user + '.' + ' Thank you for stopping by! ');
 }
 
 //Question 6
@@ -138,6 +164,7 @@ for (let i = 1; i <= 3; i++) {
   
   if (response == answer5) {
     alert('That is correct! ');
+      let count = count++;
     break;
     }
     else if (response == '') {
@@ -174,6 +201,7 @@ for (let i=0; i < 6; i++) {
     for (let answer7 of food) {
       if(response1.toLowerCase() === answer7) {
         alert('You are correct! ');
+        let count = count++;
         fave = true;
         break;
       }
@@ -188,4 +216,6 @@ for (let i=0; i < 6; i++) {
       }
        
 
-      alert('My favourite foods are ' + food[0] + ' , ' + food[1] + ' , '+ food[2] + ' , and, ' + food[3]);
+      alert('My favourite foods are ' + food[0] + ' , ' + food[1] + ' , '+ food[2] + ' , and, ' + food[3] + '.');
+
+      alert('You scored ' + count + ' out of 7! Nice Job!');
